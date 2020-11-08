3dceca02c82f3a8204f13d99ecafe4fdf576e568
Pavlo Svystun <seraf111@mail.ru>
2020-11-08 05:53:33 +0200

    new sort

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf31162f0ec261c5964e670f42510702.js"
+  "/precache-manifest.6f7935ed0b49560f6e4da64224b13b4c.js"
 );
 
 self.addEventListener('message', (event) => {
